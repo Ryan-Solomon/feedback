637e3db40747fa4bdf8b9f900200c727e670d3be
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-11 17:31:12 -0600

    add some global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import { CSSReset, ThemeProvider } from '@chakra-ui/react';
+import { css, CSSReset, ThemeProvider } from '@chakra-ui/react';
+import { Global } from '@emotion/react';
 import React from 'react';
 import { AuthProvider } from '../lib/auth';
 import theme from '../styles/theme';
@@ -8,6 +9,19 @@ function MyApp({ Component, pageProps }) {
     <ThemeProvider theme={theme}>
       <AuthProvider>
         <CSSReset />
+        <Global
+          styles={css`
+            html {
+              scroll-behavior: smooth;
+            }
+
+            #__next {
+              display: flex;
+              flex-direction: column;
+              min-height: 100vh;
+            }
+          `}
+        />
         <Component {...pageProps} />
       </AuthProvider>
     </ThemeProvider>
