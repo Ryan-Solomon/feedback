b79541a2e142059eab7ac5724873b8c753b299b6
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-10 19:54:22 -0600

    add login with github

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,11 +1,9 @@
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/functions';
 import 'firebase/firestore';
 
-//@ts-ignore
 if (!firebase.apps.length) {
-  //@ts-ignore
   firebase.initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
